527456d3cb0cd3bd54a21d4259c3564ae8a1a03f
pok125 <pokljh0411@naver.com>
2023-02-10 01:29:01 +0900

    Implementation

---
+++ Implementation_Problem/Implementation_Problem/Problem1.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+// 상하좌우 문제
+// 공간의 크기 N*N
+
+int N = 0;
+int x = 1;
+int y = 1;
+string input1 = Console.ReadLine();
+int.TryParse(input1, out N);
+
+input1 = Console.ReadLine();
+string[] moveWord = input1.Split(' ');
+
+for (int i = 0; i < moveWord.Length; i++)
+{
+    switch (moveWord[i])
+    {
+        case "L":
+            if (y - 1 >= 1)
+                y--;
+            break;
+        case "R":
+            if (y + 1 <= N)
+                y++;
+            break;
+        case "U":
+            if (x - 1 >= 1)
+                x--;
+            break;
+        case "D":
+            if (x + 1 <= N)
+                x++;
+            break;
+    }
+}
+
+Console.WriteLine("{0} {1}",x,y);
